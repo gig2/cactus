d2378c860152027c0e279b44b33c97ad70acf736
VARIN kevin <v13009110@L-059011E116-13.salsa.univ-amu.fr>
2018-11-05 14:51:47 +0100

    ajout moyenne median min max + modif valence

--- src/core/valence.cpp
+++ src/core/valence.cpp
@@ -1,6 +1,52 @@
 #include "valence.h"
 
-valence::valence()
-{
+using namespace std;
 
+Valence::Valence(const MeshT &mesh) {
+    Valence::valence_init(mesh);
+}
+
+void Valence::valence_init(const MeshT &mesh)
+{
+    valence_par_points.clear();
+    valence_par_points_trie.clear();
+    auto vIt = mesh.vertices_begin();
+    while( vIt != mesh.vertices_end() )
+        valence_par_points.push_back((int)mesh.valence(*vIt));
+
+    std::copy(std::begin(valence_par_points),std::end(valence_par_points),std::back_inserter(valence_par_points_trie));
+    std::sort(std::begin(valence_par_points_trie),std::end(valence_par_points_trie));
+}
+
+float Valence::moyenne_valence(){
+    int nb_sommet = 0;
+    int total_valence = 0;
+    for(int i = 0 ; i < valence_par_points.size() ; i++){
+        nb_sommet++;
+        total_valence += valence_par_points.at(i);
+    }
+    return ((total_valence*1.0f) / (nb_sommet*1.0f));
+
+}
+
+float Valence::mediane_valence(){
+    int npts = valence_par_points_trie.size();
+    if(npts%2 == 1)
+        return valence_par_points_trie[(npts+1)/2];
+    else {
+       int m = npts/2;
+       return ((valence_par_points_trie[m] + valence_par_points_trie[m+1])/2);
+    }
+}
+
+int Valence::min_valence(){
+    if(valence_par_points_trie.empty())
+        throw std::runtime_error("tableau de valence vide!");
+    return valence_par_points_trie.front();
+}
+
+int Valence::max_valence(){
+    if(valence_par_points_trie.empty())
+        throw std::runtime_error("tableau de valence vide!");
+    return valence_par_points_trie.back();
 }
--- src/core/valence.h
+++ src/core/valence.h
@@ -1,11 +1,31 @@
 #ifndef VALENCE_H
 #define VALENCE_H
 
+#include <vector>
+#include "mesh.h"
 
-class valence
+class Valence
 {
 public:
-    valence();
+    // pour chaque sommet est associé sa valence.
+    std::vector<int> valence_par_points;
+    std::vector<int> valence_par_points_trie;
+
+    // constructeur par défaut.
+    explicit Valence(const MeshT &mesh);
+
+    // effectue le remplissage du vector "valence_par_points".
+    void valence_init(const MeshT &mesh);
+
+    // renvois la moyenne des valences des sommets du meshage.
+    float moyenne_valence();
+
+    float mediane_valence();
+
+    int min_valence();
+
+    int max_valence();
+
 };
 
 #endif // VALENCE_H
