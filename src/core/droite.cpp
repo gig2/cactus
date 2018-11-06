684c268f8361df182c1d77272355864b1250594d
ARCELLIER Stephane <a14017947@L-059011E116-13.salsa.univ-amu.fr>
2018-11-06 13:22:10 +0100

    Algos de base droite

--- src/core/droite.cpp
+++ src/core/droite.cpp
@@ -1,6 +1,31 @@
 #include "droite.h"
 
-Droite::Droite()
+Droite::Droite(PointT ptA, PointT ptB)
 {
+    set_vecteur_directeur(ptA, ptB);
+    pt_ = ptA;
+}
 
+void Droite::set_vecteur_directeur(PointT ptA, PointT ptB){
+    vx_ = ptA.x - ptB.x;
+    vy_ = ptA.y - ptB.y;
+    vz_ = ptA.z - ptB.z;
+}
+
+bool Droite::point_dans_droite(PointT p){
+    //On obtient t en résolvant l'équation p.x = pt_.x + vx_ * t
+    int t = p.x - pt_.x / vx_;
+    int y = pt_.y + vy_ * t;
+    int z = pt_.z + vz_ * t;
+    if(y != p.y || z!= p.z){
+        return false;
+    }
+    return true;
+}
+
+float Droite::distance_point_droite(PointT p){
+    if(point_dans_droite(p)){
+        return 0.0;
+    }
+    return 1.0;
 }
--- src/core/droite.h
+++ src/core/droite.h
@@ -4,8 +4,16 @@
 
 class Droite
 {
+private:
+    //Vecteur directeur de la droite.
+    int vx_,vy_,vz_;
+    //On garde les coordonnées d'un point (fragment de la représentation paramétrique d'une droite)
+    PointT pt_;
 public:
     Droite();
+    void set_vecteur_directeur(PointT ptA, PointT ptB);
+    bool point_dans_droite(PointT p);
+    float distance_point_droite(PointT p);
 };
 
 #endif // DROITE_H
