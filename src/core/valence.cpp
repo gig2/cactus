69cce8ed673dcde584b9852955b1c77e7cf26a36
VARIN kevin <v13009110@L-059011E116-14.salsa.univ-amu.fr>
2018-11-06 13:35:17 +0100

    amelioration 1

--- src/core/valence.cpp
+++ src/core/valence.cpp
@@ -11,21 +11,20 @@ void Valence::valence_init(const MeshT &mesh)
     valence_par_points.clear();
     valence_par_points_trie.clear();
     auto vIt = mesh.vertices_begin();
-    while( vIt != mesh.vertices_end() )
+    while( vIt != mesh.vertices_end() ){
         valence_par_points.push_back((int)mesh.valence(*vIt));
+        vIt++;
+    }
 
     std::copy(std::begin(valence_par_points),std::end(valence_par_points),std::back_inserter(valence_par_points_trie));
     std::sort(std::begin(valence_par_points_trie),std::end(valence_par_points_trie));
 }
 
 float Valence::moyenne_valence(){
-    int nb_sommet = 0;
     int total_valence = 0;
-    for(int i = 0 ; i < valence_par_points.size() ; i++){
-        nb_sommet++;
+    for(int i = 0 ; i < valence_par_points.size() ; i++)
         total_valence += valence_par_points.at(i);
-    }
-    return ((total_valence*1.0f) / (nb_sommet*1.0f));
+    return ((total_valence*1.0f) / (valence_par_points.size()*1.0f));
 
 }
 
