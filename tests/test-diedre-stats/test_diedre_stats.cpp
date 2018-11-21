7ced9f7032d975f9c67e29fcd348972d398b39d9
Thibault Payet <monwarez@gmail.com>
2018-11-21 15:31:16 +0100

    put cube in shared test lib, and empty diedre

---
+++ tests/test-diedre-stats/test_diedre_stats.cpp
@@ -0,0 +1,12 @@
+#include "cube.h"
+#include "diedre_stats.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+using testing::Eq;
+
+TEST( aCube, stats )
+{
+    auto cube = constructCube();
+}
