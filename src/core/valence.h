e1cc37019673129a790170b8d723aec9325a56e3
Thibault Payet <monwarez@gmail.com>
2018-11-22 10:11:36 +0100

    wip

--- src/core/valence.h
+++ src/core/valence.h
@@ -2,25 +2,27 @@
 #define VALENCE_H
 
 #include "mesh.h"
-#include <vector>
 #include <iterator>
+#include <vector>
 
 template <typename inputIT>
-double moyenne(inputIT begin, inputIT end){
-    double nb_element = std::distance(begin, end);
-    double somme_all_valences = std::accumulate(begin, end, 0);
+double moyenne( inputIT begin, inputIT end )
+{
+    double nb_element         = std::distance( begin, end );
+    double somme_all_valences = std::accumulate( begin, end, 0 );
 
-    return somme_all_valences/nb_element;
+    return somme_all_valences / nb_element;
 }
 
 /** \pre trié entre begin et end.
-*/
+ */
 template <typename inputIT>
-double mediane_sorted(inputIT begin, inputIT end){
-    int nb_element = std::distance(begin, end);
-    auto iter = begin;
+double mediane_sorted( inputIT begin, inputIT end )
+{
+    int nb_element = std::distance( begin, end );
+    auto iter      = begin;
     if ( nb_element % 2 == 1 )
-        return *(iter+(nb_element+1)/2);
+        return *( iter + ( nb_element + 1 ) / 2 );
     else
     {
         int m = nb_element / 2, med = 0;
@@ -30,23 +32,24 @@ double mediane_sorted(inputIT begin, inputIT end){
         med += *iter;
         return med;
     }
-
 }
 
 /** \pre trié entre begin et end.
-*/
+ */
 template <typename inputIT>
-int min_sorted(inputIT begin, inputIT end){
-    auto last = end-1;
-    return std::min(*begin, *last);
+int min_sorted( inputIT begin, inputIT end )
+{
+    auto last = end - 1;
+    return std::min( *begin, *last );
 }
 
 /** \pre trié entre begin et end.
-*/
+ */
 template <typename inputIT>
-int max_sorted(inputIT begin, inputIT end){
-    auto last = end-1;
-    return std::max(*begin, *last);
+int max_sorted( inputIT begin, inputIT end )
+{
+    auto last = end - 1;
+    return std::max( *begin, *last );
 }
 
 class Valence
@@ -56,24 +59,16 @@ public:
 
     std::vector<int> valence_triee;
 
-    auto begin(){
-        return std::begin(valence_triee);
-    }
-    auto end(){
-        return std::end(valence_triee);
-    }
+    auto begin() { return std::begin( valence_triee ); }
+    auto end() { return std::end( valence_triee ); }
 
-    auto cbegin() const{
-        return std::begin(valence_triee);
-    }
-    auto cend() const{
-        return std::end(valence_triee);
-    }
+    auto cbegin() const { return std::begin( valence_triee ); }
+    auto cend() const { return std::end( valence_triee ); }
 
     // constructeur par défaut.
     explicit Valence( const MeshT &mesh );
 
-    int getValenceOfPoint(int pointID) const;
+    int getValenceOfPoint( int pointID ) const;
 
     // renvois la moyenne des valences des sommets du meshage.
 
@@ -82,7 +77,7 @@ private:
     // effectue le remplissage du vector "valence_par_points".
     void valence_init( const MeshT &mesh );
 
-    const MeshT meshview;
+    const MeshT &meshview;
 };
 
 #endif // VALENCE_H
